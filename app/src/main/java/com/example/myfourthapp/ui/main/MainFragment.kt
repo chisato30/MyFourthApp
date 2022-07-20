43735520a9957571e3e7829f682546c2f354e954
chisato30 <moterunit@gmail.com>
2022-07-20 23:03:35 +0900

    SubFragment作成

--- app/src/main/java/com/example/myfourthapp/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/myfourthapp/ui/main/MainFragment.kt
@@ -6,7 +6,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.example.myfourthapp.R
+import com.example.myfourthapp.databinding.MainFragmentBinding
 
 class MainFragment : Fragment() {
 
@@ -15,18 +17,24 @@ class MainFragment : Fragment() {
     }
 
     private lateinit var viewModel: MainViewModel
+    private lateinit var binding: MainFragmentBinding
+
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
+        binding = DataBindingUtil.inflate(inflater,
+        R.layout.main_fragment, container, false)
+        binding.setLifecycleOwner(this)
+        return  binding.getRoot()
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
+        viewModel = ViewModelProvider(this)
+            .get(MainViewModel::class.java)
+        binding.viewmodel = viewModel
     }
-
 }
